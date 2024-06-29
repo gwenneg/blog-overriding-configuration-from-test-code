a15576db7dc1206bf0664539163fa14349ec5d90
Gwenneg Lepage <glepage@redhat.com>
2024-06-29 17:16:29 +0200

    Add native testing to profile-aware-file module

---
+++ profile-aware-file/src/test/java/com/gwenneg/blog/config/FeaturesResourceIT.java
@@ -0,0 +1,8 @@
+package com.gwenneg.blog.config;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class FeaturesResourceIT extends FeaturesResourceTest {
+    // Execute the same tests but in packaged mode.
+}
